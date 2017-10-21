d46989de9b1339ee0770f5a759a9f7a45af83b5a
Aayush Sharma <greatash90@gmail.com>
2017-10-21 09:29:12 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const config = require('/home/l33t/Documents/practise/mean/meanAlpha/config.js');
 
 var DB = require('../db');
-var uri = "mongodb://aayush24:sharmaji@meanbeta-shard-00-00-sshqm.mongodb.net:27017,meanbeta-shard-00-01-sshqm.mongodb.net:27017,meanbeta-shard-00-02-sshqm.mongodb.net:27017/users?ssl=true&replicaSet=MEANbeta-shard-0&authSource=admin";
+var uri = "mongodb://aayush24:xxxxxx@meanbeta-shard-00-00-sshqm.mongodb.net:27017,meanbeta-shard-00-01-sshqm.mongodb.net:27017,meanbeta-shard-00-02-sshqm.mongodb.net:27017/users?ssl=true&replicaSet=MEANbeta-shard-0&authSource=admin";
 
 
 router.get( '/', (req, res) => {
